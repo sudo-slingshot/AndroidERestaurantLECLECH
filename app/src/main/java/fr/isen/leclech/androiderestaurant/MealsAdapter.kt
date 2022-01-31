de69a899f3fab4754a7516c77fd4e2755eb9d145
sudo-slingshot <76110906+sudo-slingshot@users.noreply.github.com>
2022-01-31 16:30:53 +0100

    Price implementation commit
    Includes price display implementation
    
    TODO : display the images

--- app/src/main/java/fr/isen/leclech/androiderestaurant/MealsAdapter.kt
+++ app/src/main/java/fr/isen/leclech/androiderestaurant/MealsAdapter.kt
@@ -1,17 +1,23 @@
 package fr.isen.leclech.androiderestaurant;
 
+import android.icu.number.NumberFormatter.with
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView;
+import com.squareup.picasso.Picasso
 import fr.isen.leclech.androiderestaurant.databinding.CellMealBinding;
+import java.lang.System.load
 
 
 class MealsAdapter(private val entries: List<Dish>, val itemClickListener: (Dish) -> Unit): RecyclerView.Adapter<MealsAdapter.MealsViewHolder>() {
     class MealsViewHolder(binding:CellMealBinding): RecyclerView.ViewHolder(binding.root){
         val title=binding.mealTitle
+        //val image=binding.mealImage
+        val price= binding.mealPrice
         val layout = binding.root
         fun bind (meal:Dish){
             title.text=meal.name
+            price.text="${meal.prices.first().price} €"
         }
     }
 
