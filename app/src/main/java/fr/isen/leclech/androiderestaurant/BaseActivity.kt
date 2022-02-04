54d59be9ca2829b7f37a2cfecafa163a0729dc43
sudo-slingshot <76110906+sudo-slingshot@users.noreply.github.com>
2022-02-04 13:41:56 +0100

    Front design update
    Front design update

--- app/src/main/java/fr/isen/leclech/androiderestaurant/BaseActivity.kt
+++ app/src/main/java/fr/isen/leclech/androiderestaurant/BaseActivity.kt
@@ -7,11 +7,14 @@ import android.view.Menu
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import com.google.android.material.snackbar.Snackbar
 import fr.isen.leclech.androiderestaurant.Basket.Basket
 import fr.isen.leclech.androiderestaurant.Basket.BasketActivity
+import fr.isen.leclech.androiderestaurant.databinding.ActivityHomeBinding
 
 open class BaseActivity: AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
         menuInflater.inflate(R.menu.menu, menu)
         val menuView= menu?.findItem(R.id.basket)?.actionView
         val countText=menuView?.findViewById(R.id.itemCount) as? TextView
@@ -24,6 +27,8 @@ open class BaseActivity: AppCompatActivity() {
             if (count > 0) {
                 val intent = Intent(this, BasketActivity::class.java)
                 startActivity(intent)
+            }else{
+                Snackbar.make(menuView, R.string.emptycart, Snackbar.LENGTH_LONG).show()
             }
         }
 
